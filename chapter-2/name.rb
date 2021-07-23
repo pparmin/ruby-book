2fb6d8898b8e0ff709f1ee693a3b8153f5fc30c0
Philipp Armingeon <philipp.armingeon@googlemail.com>
2021-07-23 17:02:48 +0200

    add exercises for chapter 2

---
+++ chapter-2/name.rb
@@ -0,0 +1,7 @@
+puts "Please type in your first name!"
+first_name = gets.chomp
+puts "Please type in your last name!"
+last_name = gets.chomp
+puts "Welcome #{first_name} #{last_name}"
+
+10.times { puts first_name + " " + last_name }
