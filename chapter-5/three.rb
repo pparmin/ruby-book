28df426e6afb2b03780edb8387707351cbf2ea26
Philipp Armingeon <philipp.armingeon@googlemail.com>
2021-07-25 00:55:12 +0200

    add exercises for chapter 5

---
+++ chapter-5/three.rb
@@ -0,0 +1,8 @@
+def countdown(number)
+  puts number
+  if number > 0 
+    countdown(number-1)
+  end
+end
+
+countdown(6)
---
+++ chapter-5/two.rb
@@ -0,0 +1,6 @@
+input = ""
+
+while input != "STOP" do 
+  puts "Please enter sth"
+  input = gets.chomp
+end
